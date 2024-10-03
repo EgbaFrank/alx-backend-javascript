f89abd1be08e2b658616b14f863ae78d795469d7
Egba Frank <Egbafrank72@gmail.com>
2024-10-03 22:15:20 +0000

    improved logic

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+	try {
+		return {
+			photo: await uploadPhoto(),
+			user: await createUser(),
+		}
+	} catch {(err) => ({
+		photo: null,
+		user: null,
+		})
+	};
+}
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([photoData, userData]) => {
     console.log(photoData.body, userData.firstName, userData.lastName);
-  }).catch((err) => {
-    console.error('Signup system offline', err);
-  });
+  }).catch((err) => console.log('Signup system offline'));
 }
