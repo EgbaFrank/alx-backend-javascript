bd68281ee77fc07396ee146e6f8f2919c7f381b7
Egba Frank <Egbafrank72@gmail.com>
2024-10-03 22:55:53 +0000

    improve style

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([photoData, userData]) => {
     console.log(photoData.body, userData.firstName, userData.lastName);
-  }).catch((err) => console.log('Signup system offline'));
+  }).catch(() => console.log('Signup system offline'));
 }
