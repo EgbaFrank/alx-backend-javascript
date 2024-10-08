9b6136ec659a73547ed30353fc573bc956944ae4
Egba Frank <Egbafrank72@gmail.com>
2024-10-08 20:54:10 +0000

    fix undefined variable use

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(studentList) {
   if (!Array.isArray(studentList)) {
     return [];
   }
-  return objArr.map((student) => student.id);
+  return studentList.map((student) => student.id);
 }
