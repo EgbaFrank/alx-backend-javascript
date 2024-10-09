cb4303be9965dff1ee85ff8292d9c37b19882516
Egba Frank <Egbafrank72@gmail.com>
2024-10-09 02:41:28 +0000

    add more input checks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !set.size) {
-    return ' ';
+  if (!startString || !set || typeof startString !== 'string' || !(set instanceof Set)) {
+    return '';
   }
 
-  const filteredValues = [...set].filter((val) => val.startsWith(startString));
+  const filteredValues = [...set].filter((val) => typeof val === 'string' && val.startsWith(startString));
   return filteredValues.map((val) => val.slice(startString.length)).join('-');
 }
