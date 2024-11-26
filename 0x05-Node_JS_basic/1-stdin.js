8ea5285bfc3c59f45cf7abf8fa372d93b59a2498
Egba Frank <Egbafrank72@gmail.com>
2024-11-26 23:59:27 +0000

    removed comment

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,4 @@
-// interact with user
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
