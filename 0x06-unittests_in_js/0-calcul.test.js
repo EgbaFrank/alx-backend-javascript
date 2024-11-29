69368292a06eccdc620307461b61a8f742a9f251
Egba Frank <Egbafrank72@gmail.com>
2024-11-29 09:09:39 +0000

    Add more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,14 +5,17 @@ describe('calculateNumber test', () => {
   it('should add whole numbers correctly', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should round-up and add numbers correctly', () => {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-  it('should round-up and add numbers correctly', () => {
+  it('should round-up a and add numbers correctly', () => {
     assert.strictEqual(calculateNumber(3.7, 1), 5);
   });
-  it('should round-down and add numbers correctly', () => {
+  it('should round-up b and add numbers correctly', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should round-down a and add numbers correctly', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should round-down b and add numbers correctly', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.3), 4);
   });
   it('should round and add decimal numbers correctly', () => {
     assert.strictEqual(calculateNumber(1.7, 3.7), 6);
