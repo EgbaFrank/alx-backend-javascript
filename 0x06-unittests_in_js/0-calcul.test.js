3b52f00e0683f941cab94af5c245c1324cfd4d54
Egba Frank <Egbafrank72@gmail.com>
2024-11-29 09:03:22 +0000

    Add / test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,8 +8,11 @@ describe('calculateNumber test', () => {
   it('should round-up and add numbers correctly', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
+  it('should round-up and add numbers correctly', () => {
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+  });
   it('should round-down and add numbers correctly', () => {
-    assert.strictEqual(calculateNumber(1.2, 3), 4);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('should round and add decimal numbers correctly', () => {
     assert.strictEqual(calculateNumber(1.7, 3.7), 6);
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -4,6 +4,14 @@ const request = require('request');
 const baseUrl = 'http://localhost:7865';
 
 describe('integration tests', () => {
+  it('should return home route', (done) => {
+    request(baseUrl, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it('should return cart payment methods', (done) => {
     request(`${baseUrl}/cart/12`, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
